eae22f775a62ae060f793a44958a3fbebf70a3b7
kshitiz vikram singh <kshitizvikramsingh123@gmail.com>
2023-08-19 14:47:55 +0530

    added depth to reach us map

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -2,13 +2,22 @@
 
 
  console.log("script is loaded")
- //mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xsY3F5cnltMDNvNDNjdGo4OXNxYjc1cSJ9.B6G-l78V8Ag076eGaroswQ';
-//  const map = new mapboxgl.Map({
-//  container: 'map', // container ID
-//  style: 'mapbox://styles/mapbox/dark-v11', // style URL
-//  center: [ 77.4771321,28.7145463 ], // starting position [lng, lat]
-//  zoom: 16, // starting zoom
-//  });
+ const geojson = {
+    'type': 'FeatureCollection',
+    'features': [
+    {
+    'type': 'Feature',
+    'properties': {
+    'message': 'Manokamna Dham',
+    
+    },
+    'geometry': {
+    'type': 'Point',
+    'coordinates': [77.4771321, 28.7145463]
+    }
+    }
+    ]
+    };
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
 const map = new mapboxgl.Map({
@@ -16,76 +25,234 @@ container: 'map',
 // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
 style: 'mapbox://styles/mapbox/streets-v12',
 center: [77.4771321, 28.7145463],
-zoom: 13
+zoom: 0
 });
-const marker1 = new mapboxgl.Marker()
-.setLngLat([77.4771321, 28.7145463])
-.addTo(map);
-map.addControl(
-new MapboxDirections({
-accessToken: mapboxgl.accessToken
-}),
-'top-left'
-);
+ 
+
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 1
+    });
+    //your code to be executed after 1 second
+}, 500);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 2
+    });
+    //your code to be executed after 1 second
+}, 1000);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 3
+    });
+    //your code to be executed after 1 second
+}, 1500);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 4
+    });
+    //your code to be executed after 1 second
+}, 2000);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 5
+    });
+    //your code to be executed after 1 second
+}, 2500);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 6
+    });
+    //your code to be executed after 1 second
+}, 3000);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 7
+    });
+    //your code to be executed after 1 second
+}, 3500);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 8
+    });
+    //your code to be executed after 1 second
+}, 4000);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 9
+    });
+    //your code to be executed after 1 second
+}, 4500);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 10
+    });
+    //your code to be executed after 1 second
+}, 5000);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 11
+    });
+    //your code to be executed after 1 second
+}, 5500);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 12
+    });
+    //your code to be executed after 1 second
+}, 6000);
+setTimeout(function() {
+    mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
+    const map = new mapboxgl.Map({
+    container: 'map',
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [77.4771321, 28.7145463],
+    zoom: 15
+    });
+//     const marker1 = new mapboxgl.Marker()
+// .setLngLat([77.4771321, 28.7145463])
+// .addTo(map);
+// // map.addControl(
+// // new MapboxDirections({
+// // accessToken: mapboxgl.accessToken
+// // }),
+// // 'top-left'
+// // );
+    //your code to be executed after 1 second
 
 
-// mapboxgl.accessToken = 'pk.eyJ1Ijoia3NoaXRpejIxMDQiLCJhIjoiY2xnMjYwNHpzMDJiNzNubG8xZGszOXdnZyJ9.wd3pEFZuVlguNBkk7NPKiA';
-// const map = new mapboxgl.Map({
-// container: 'map',
-// // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-// style: 'mapbox://styles/mapbox/streets-v12',
-// center: [77.4771321, 28.7145463],
-// zoom: 14
-// });
- 
-// map.on('load', () => {
-// // Add an image to use as a custom marker
-// map.loadImage(
-// 'https://docs.mapbox.com/mapbox-gl-js/assets/custom_marker.png',
-// (error, image) => {
-// if (error) throw error;
-// map.addImage('custom-marker', image);
-// // Add a GeoJSON source with 2 points
-// map.addSource('points', {
-// 'type': 'geojson',
-// 'data': {
-// 'type': 'FeatureCollection',
-// 'features': [
-// {
-// // feature for Mapbox DC
-// 'type': 'Feature',
-// 'geometry': {
-// 'type': 'Point',
-// 'coordinates': [
-//     77.4771321, 28.7145463
-// ]
-// },
-// 'properties': {
-// 'title': 'Mapbox Gzb'
-// }
-// }
-// ]
-// }
-// });
- 
-// // Add a symbol layer
-// map.addLayer({
-// 'id': 'points',
-// 'type': 'symbol',
-// 'source': 'points',
-// 'layout': {
-// 'icon-image': 'custom-marker',
-// // get the title name from the source's "title" property
-// 'text-field': ['get', 'title'],
-// 'text-font': [
-// 'Open Sans Semibold',
-// 'Arial Unicode MS Bold'
-// ],
-// 'text-offset': [0, 1.25],
-// 'text-anchor': 'top'
-// }
-// });
-// }
-// );
-// });
- 
+    map.on('load', () => {
+        map.addSource('places', {
+        // This GeoJSON contains features that include an "icon"
+        // property. The value of the "icon" property corresponds
+        // to an image in the Mapbox Streets style's sprite.
+        'type': 'geojson',
+        'data': {
+        'type': 'FeatureCollection',
+        'features': [
+        {
+        'type': 'Feature',
+        'properties': {
+        'description':
+        '<strong>Manokamna Dham</strong><p>Always Open</p>',
+        'icon': 'art-gallery'
+        },
+        'geometry': {
+        'type': 'Point',
+        'coordinates': [77.4771321, 28.7145463]
+        }
+    }]
+        }
+        })
+    
+    
+        map.addLayer({
+        'id': 'places',
+        'type': 'symbol',
+        'source': 'places',
+        'layout': {
+        'icon-image': ['get', 'icon'],
+        'icon-allow-overlap': true
+        }
+        });
+    
+        // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+        map.on('click', 'places', (e) => {
+        // Copy coordinates array.
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties.description;
+         
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+         
+        new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
+        });
+    
+        // Change the cursor to a pointer when the mouse is over the places layer.
+        map.on('mouseenter', 'places', () => {
+        map.getCanvas().style.cursor = 'pointer';
+        });
+         
+        // Change it back to a pointer when it leaves.
+        map.on('mouseleave', 'places', () => {
+        map.getCanvas().style.cursor = '';
+        });
+        });
+}, 6500);
+// Add markers to the map.
+
+
+
+
+
+
+    
